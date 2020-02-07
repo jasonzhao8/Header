1b3d30566711f8af81520405c872139c3e671a26
jasonzhao8 <jason.zhao@rbe.sk.ca>
2020-02-07 08:53:45 -0600

    Create Practice.py
    Jason Zhao

---
+++ Practice.py
@@ -0,0 +1,12 @@
+# Course: CS 30
+# Period 1
+# Date Created: 06/02/20
+# Last Modified: 06/02/20
+# Name: Jason Zhao
+# Description: This program runs to solve physics equations
+
+
+time = "t"
+
+
+eqn = ()
